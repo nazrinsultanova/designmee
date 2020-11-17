fe8be840dd0be40b137c1173d78a7d6d87e062c2
nazrinsultanova <nezrin.sultan2001@gmail.com>
2020-11-17 17:32:52 +0400

    Portfolio

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){ 
+    $('.image-container img').attr('data-fancybox','gallery');
+ 
+     
+    $('.image-container img').fancybox({
+         loop : true
+     });
+})
